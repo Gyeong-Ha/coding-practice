fba4a9e69376327b37d6505e9cb7b87c9a3ddd19
Gyeong-Ha <29hagyeong@gmail.com>
2024-09-28 16:12:55 +0900

    [Bronze III] Title: ACM 호텔, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,13 +1,15 @@
 T = int(input())
-for t in range(0, T):
-    i = 0
+
+for t in range(T):
     H, W, N = map(int, input().split())
-    for w in range(1, W+1):
-        for h in range(1, H+1):
-            i += 1
-            if i == N:
-                answer = h*100 + w
-                print(answer)
-                continue
-            else:
-                pass
+
+    n = 0
+    while True:
+        for width in range(1, W+1):
+            for height in range(1, H+1):
+                if n < N:
+                    n += 1
+                    h_answer = height
+                    w_answer = width
+        break
+    print(h_answer*100+w_answer)
