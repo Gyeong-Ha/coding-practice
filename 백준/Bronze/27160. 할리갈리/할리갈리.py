4b03baaeefce131c308f42edcb1324cfdd924785
Gyeong-Ha <29hagyeong@gmail.com>
2024-11-02 18:56:52 +0900

    [Bronze II] Title: 할리갈리, Time: 2636 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/27160. 할리갈리/할리갈리.py
+++ 백준/Bronze/27160. 할리갈리/할리갈리.py
@@ -5,14 +5,7 @@ for i in range(n):
     lt = list(input().split())
     dic[lt[0]] += int(lt[1])
 
-for x in sorted(list(dic.values())):
-    if x == 5:
-        answer = 1
-        break
-    else:
-        answer = 0
-
-if answer == 1:
-    print("YES")
+if 5 in dic.values():
+    print('YES')
 else:
-    print("NO")
+    print('NO')
